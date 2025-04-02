d73e3ab47342b6bcaab738c699a7ed5461ef02ef
EnergyPatrikHU <energypatrikhu@proton.me>
2025-04-02 22:42:36 +0200

    Add JSDoc comments to User entity properties for better documentation

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,5 +1,17 @@
 export class User {
+  /**
+   * The name of the user
+   */
   name: string;
+
+  /**
+   * The email of the user
+   */
   email: string;
+
+  /**
+   * The role of the user,
+   * this can be 'USER' or 'WORKER'
+   */
   role: string;
 }
