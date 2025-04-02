8f677356a0ca361567ecd6ba2ef506ca0b21be9f
EnergyPatrikHU <energypatrikhu@proton.me>
2025-04-02 22:42:49 +0200

    Add BlogModule to AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,15 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { ReservationsModule } from './reservations/reservations.module';
+import { BlogModule } from './blog/blog.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, ProductsModule, ReservationsModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    ProductsModule,
+    ReservationsModule,
+    BlogModule,
+  ],
 })
 export class AppModule {}
