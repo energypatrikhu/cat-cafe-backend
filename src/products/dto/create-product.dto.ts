8939890029076f394b469466fcb2c6d37f0fc400
EnergyPatrikHU <energypatrikhu@proton.me>
2025-04-02 15:35:10 +0200

    Add validation rules to CreateProductDto for improved data integrity

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,16 @@
-export class CreateProductDto {}
+import { IsInt, IsNumber, IsPositive, IsString } from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  name: string;
+
+  @IsString()
+  description: string;
+
+  @IsNumber()
+  price: number;
+
+  @IsInt()
+  @IsPositive()
+  quantity: number;
+}
