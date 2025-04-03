2a677eb5717a20d49f17754a95e4c53347b357a0
EnergyPatrikHU <energypatrikhu@proton.me>
2025-04-03 15:21:16 +0200

    Reorder imports in seed script for consistency

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,5 @@
-import { PrismaClient } from '@prisma/client';
 import { faker } from '@faker-js/faker';
+import { PrismaClient } from '@prisma/client';
 import { hash } from 'argon2';
 
 const prisma = new PrismaClient();
