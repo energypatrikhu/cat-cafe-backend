661ba7be0e7ffcdc4199cec40a268a3049c9d8f5
EnergyPatrikHU <energypatrikhu@proton.me>
2025-04-04 12:03:37 +0200

    Enhance BuyProductDto to include validation for product quantity and nested product lists

--- src/products/dto/buy-product.dto.ts
+++ src/products/dto/buy-product.dto.ts
@@ -1,15 +1,25 @@
-import { IsInt, IsPositive } from 'class-validator';
+import { IsInt, IsPositive, ValidateNested } from 'class-validator';
 
 class BuyProduct {
+  /**
+   * Product ID
+   */
   @IsInt()
   @IsPositive()
   id: number;
 
+  /**
+   * Product quantity
+   */
   @IsInt()
   @IsPositive()
   quantity: number;
 }
 
 export class BuyProductDto {
+  /**
+   * List of products to buy
+   */
+  @ValidateNested({ each: true })
   products: BuyProduct[];
 }
