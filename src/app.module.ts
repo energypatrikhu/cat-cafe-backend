96819208d175e53edae7bf909cc7a654edb61fe0
EnergyPatrikHU <energypatrikhu@proton.me>
2025-04-09 13:14:14 +0200

    Remove Blog module and related components

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
-import { BlogModule } from './blog/blog.module';
 import { ProductsModule } from './products/products.module';
 import { ReservationsModule } from './reservations/reservations.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [
-    AuthModule,
-    UsersModule,
-    ProductsModule,
-    ReservationsModule,
-    BlogModule,
-  ],
+  imports: [AuthModule, UsersModule, ProductsModule, ReservationsModule],
 })
 export class AppModule {}
